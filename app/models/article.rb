807101be7ac1f377e0a6327fdce9619d43395c41
“Kyle <kschmit90@gmail.com>
2014-09-10 14:34:02 -0500

    removed methods from article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,22 +1,4 @@
 class Article < ActiveRecord::Base
   attr_accessible :body, :draft, :title
   validates :body, :title, presence: true
-  
-  def excerpt(string)
-      excerptarray = string.split("")
-      excerptarray[0...29].join("")
-    end
-  
-    def relative_length
-      @article = Article.find(params[:id])
-      wordsarray = @article.body.split(" ")
-      if wordsarray >= 500
-        return "Long"
-      elsif (wordsarray >= 100) && (wordsarray < 500)
-        return "medium"
-      else wordsarray <= 99
-        return "short"
-      end
-    end
-    
 end
