dee3324bca59604983fdd6b0261624c82b850229
“Kyle <kschmit90@gmail.com>
2014-09-11 10:15:30 -0500

    added excerpt method to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
 class Article < ActiveRecord::Base
   attr_accessible :body, :draft, :title
   validates :body, :title, presence: true
+  
+  def excerpt(body)
+    excerptarray = body.split("")
+    excerptarray[0...29].join("")
+  end
+  
+  def relative_length
+  end
+  
 end
